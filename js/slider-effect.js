981d27c4c1fdf2a8a33842ff05f50641ab2dd902
RobertBorbely <respectsas@gmail.com>
2015-02-15 12:37:12 +0100

    Base status

---
+++ js/slider-effect.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+	$('#first').fadeIn(1500).css({display: 'table',position: 'absolute'});
+	$('#home').click(function (e){
+		var currentActivePage = $('.page-active');
+		var nextActivePage = $('#home-page');
+		var currentActiveLink = $('.active');
+
+		currentActivePage.hide('fade',500).switchClass('page-active','page-hidden');
+		nextActivePage.delay(500).show('fade',500).switchClass('page-hidden', 'page-active');
+
+		currentActiveLink.removeClass('active');
+		$(this).addClass('active');
+
+		e.preventDefault();
+	});
+
+	$('#portfolio').click(function (e){
+		var currentActivePage = $('.page-active');
+		var nextActivePage = $('#portfolio-page');
+		var currentActiveLink = $('.active');
+
+		currentActivePage.hide('fade',500).switchClass('page-active','page-hidden');
+		nextActivePage.delay(500).show('fade',500).switchClass('page-hidden', 'page-active');
+
+		currentActiveLink.removeClass('active');
+		$(this).addClass('active');
+
+		e.preventDefault();
+	});
+
+	$('#about').click(function (e){
+		var currentActivePage = $('.page-active');
+		var nextActivePage = $('#about-page');
+		var currentActiveLink = $('.active');
+
+		currentActivePage.hide('fade',500).switchClass('page-active','page-hidden');
+		nextActivePage.delay(500).show('fade',500).switchClass('page-hidden', 'page-active');
+
+		currentActiveLink.removeClass('active');
+		$(this).addClass('active');
+
+		e.preventDefault();
+	});
+
+	$('#contact').click(function (e){
+		var currentActivePage = $('.page-active');
+		var nextActivePage = $('#contact-page');
+		var currentActiveLink = $('.active');
+
+		currentActivePage.hide('fade',500).switchClass('page-active','page-hidden');
+		nextActivePage.delay(500).show('fade',500).switchClass('page-hidden', 'page-active');
+
+		currentActiveLink.removeClass('active');
+		$(this).addClass('active');
+
+		e.preventDefault();
+	});
+	
+
+});
