55dd74c4671a038bae938a737e3fd099710afefe
RobertBorbely <respectsas@gmail.com>
2015-02-21 12:03:57 +0100

    About page

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {*/
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	<link rel="icon" href="../../favicon.ico">
+	<link rel="icon" href="favicons.ico">
 	<title>Code Bandit | Reference Template</title>
 	<!-- Bootstrap core CSS -->
 	<link href="css/bootstrap.min.css" rel="stylesheet">
@@ -55,16 +55,8 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {*/
 	<!-- Custom styles for this template -->
 	<link href="css/cover.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
-	<!--<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">-->
 	<link rel="stylesheet" href="css/hover-min.css">
 
-	<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-	<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
 </head>
 <body>
 
@@ -73,7 +65,7 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {*/
 		<div class="cover-container">
 			<div class="masthead clearfix">
 				<div class="inner">
-					<h2 class="masthead-brand">Code Bandit</h2>
+					<a href="index.php"><img src="img/CodeBandit_logo.png" alt="codebandit_logo" class="masthead-brand"/></a>
 					<nav role="navigation">
 						<ul class="nav masthead-nav">
 							<li id="home" class="active hvr-pulse-grow"><a href="#home-page" data-toggle="tooltip" data-placement="bottom" title="Home">
@@ -126,58 +118,17 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {*/
 			</section>
 
 			<section id="about-page" class="inner cover page-hidden">
-				<h1>About</h1>
-			</section>
-
-			<!--<section id="contact-page" class="inner cover page-hidden">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-6 col-md-offset-3">
-								<h1 class="page-header text-center">Kapcsolat</h1>
-							<form class="form-horizontal" role="form" method="post" action="index.php">
-								<div class="form-group">
-									<label for="name" class="col-sm-2 control-label">Név</label>
-									<div class="col-sm-10">
-										<input type="text" class="form-control" id="name" name="name" placeholder="Neve" value="<?php echo htmlspecialchars($_POST['name']); ?>">
-										<?php echo "<p class='text-danger'>$errName</p>";?>
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="email" class="col-sm-2 control-label">Email</label>
-									<div class="col-sm-10">
-										<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
-										<?php echo "<p class='text-danger'>$errEmail</p>";?>
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="message" class="col-sm-2 control-label">Message</label>
-									<div class="col-sm-10">
-										<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
-										<?php echo "<p class='text-danger'>$errMessage</p>";?>
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
-									<div class="col-sm-10">
-										<input type="text" class="form-control" id="human" name="human" placeholder="Válasz">
-										<?php echo "<p class='text-danger'>$errHuman</p>";?>
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="col-sm-10 col-sm-offset-2">
-										<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="col-sm-10 col-sm-offset-2">
-										<?php echo $result; ?>
-									</div>
-								</div>
-							</form>
+				<div class="container">
+						<div class="col-md-12 picture">
+							
 						</div>
-					</div>
+						<div class="col-md-12">
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor felis, consequat ut eleifend id, feugiat quis ex. Fusce feugiat rutrum mauris eu feugiat. Sed ultricies rhoncus ipsum, sed rutrum metus dignissim eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc tellus tortor, luctus ac tristique at, maximus vel ante. Morbi non imperdiet nibh. Quisque non elit aliquet, condimentum est non, faucibus odio. Nam tincidunt, orci at iaculis maximus, tellus felis fermentum odio, eget lobortis mauris odio ut quam. Suspendisse sed lorem at ex varius lacinia non in nisl. Suspendisse placerat diam nec lacus interdum aliquam. Aenean ultrices augue dui, a sodales nunc tincidunt vitae. Sed pulvinar quam in urna semper, sed pretium augue pulvinar. Fusce accumsan arcu nec orci lobortis auctor.
+</p>
+						</div>
 				</div>
-			</section>-->
+			</section>
 
 			<section id="contactModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="contactModalLabel" area-hidden="true">
 				<div class="modal-dialog">
